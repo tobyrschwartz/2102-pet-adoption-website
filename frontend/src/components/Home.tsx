7d5a5cd54de55d6fd7cde4d2e61b6cbf7452daac
trs19009 <toby.schwartz@uconn.edu>
2025-04-22 16:31:31 -0400

    made home page pretty

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,13 +1,24 @@
 //import React from "react";
 import { Link } from "react-router-dom";
-
 const Home = () => {
     return (
-        <div>
-            <h1>Adopt a pet today!</h1>
-            <p>Click on a type of pet to get started.</p>
-            <Link to="/login">Login</Link>
+        <div style={{ textAlign: "center", padding: "20px" }}>
+            <h1>Welcome to Pet Adoption</h1>
+            <p>Your journey to finding a new friend starts here.</p>
+            <Link to="/login" style={{ textDecoration: "none" }}>
+                <button style={{ 
+                    borderRadius: "20px", 
+                    padding: "10px 20px", 
+                    backgroundColor: "#007BFF", 
+                    color: "white", 
+                    border: "none", 
+                    cursor: "pointer" 
+                }}>
+                    Login
+                </button>
+            </Link>
         </div>
     );
 };
+
 export default Home;
