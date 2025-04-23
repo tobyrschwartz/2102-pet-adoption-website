fff0827f6b96c14c78dd3b55cc2aa26ee81b2e2a
Dominic <dominic.rossi@uconn.edu>
2025-04-23 13:56:30 -0400

    Added navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/Login'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
   return (
     <Router>
       <div>
+        <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element = {<Login />} />
